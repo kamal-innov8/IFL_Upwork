c48893f7245ceb21017fffa21465eed077f0792e
DESKTOP-QK0H43P\kanna <kannanjan21@gmail.com>
2018-06-13 15:15:29 +0530

    firebase bug fixes.

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,6 +1,15 @@
-﻿using UnityEngine;
+﻿using Firebase.Database;
+using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    private void Start()
+    {
+        new Get("games", Got);
+    }
 
+    private void Got(object sender, ChildChangedEventArgs e)
+    {
+        print(e.Snapshot.GetRawJsonValue());
+    }
 }
